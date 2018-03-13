c3c16837412b6ee311765d3059af55e185db5fb7
Matthias Hölzl <tc@xantira.com>
2018-03-13 13:11:31 +0100

    Add example for using Google Test with Boost Units

---
+++ Readme.md
@@ -0,0 +1,5 @@
+C++-Tests
+=========
+
+Some simple tests and examples, mostly for Boost libraries, GTest and the
+C++-FitNesse fixture.
---
+++ SimpleGTest/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <boost/units/systems/si.hpp>
+#include <boost/units/systems/si/io.hpp>
+#include <boost/units/systems/cgs.hpp>
+#include <boost/units/systems/cgs/io.hpp>
+#include <boost/units/operators.hpp>
+
+using namespace boost::units;
+using namespace boost::units::si;
+using namespace boost::units::cgs;
+
+bool printLengths()
+{
+    auto one_meter{ 1.0 * meter };
+    auto one_hundred_centimeters{ 100.0 * centimeters};
+
+    std::cout << "1 m = " << one_meter << " = " << one_hundred_centimeters << std::endl;
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    printLengths();
+    return 0;
+}
