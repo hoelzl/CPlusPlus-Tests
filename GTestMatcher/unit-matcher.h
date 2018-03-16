aeb41175bef2aa140b5a004424c2b1b6e43fa9e4
Matthias Hoelzl <tc@xantira.com>
2018-03-16 17:45:05 +0100

    Add UnitsEqUlp matcher

--- GTestMatcher/unit-matcher.h
+++ GTestMatcher/unit-matcher.h
@@ -6,6 +6,9 @@
 #include <boost/units/systems/si.hpp>
 #include <boost/units/systems/si/io.hpp>
 #include <boost/units/operators.hpp>
+#include <boost/math/special_functions/next.hpp>
+
+
 #include "gmock/gmock.h"
 #include "gmock/gmock-matchers.h"
 #include <cmath>
@@ -14,16 +17,24 @@
 
 #include <iostream>
 
+
+
 namespace testing {
 
     constexpr double tolerance{ 2.0 };
 
-    MATCHER_P(UnitsEq, arg2, std::string("Checks if two values with units are") + std::string(negation ? " not " : "") + std::string("nearly equal"))
+    MATCHER_P(UnitsEq, arg2, std::string("Checks if two values with units are ") + std::string(negation ? "not " : "") + std::string("nearly equal"))
     {
         // std::cout << "Comparing " << arg << " and " << value << std::endl;
 
-        using arg_value_type = typename arg2_type::value_type;
-        arg_value_type diff{ std::abs(arg.value() - arg2.value()) };
-        return diff < tolerance * std::numeric_limits<arg_value_type>::epsilon();
+        using arg2_value_type = typename arg2_type::value_type;
+        arg2_value_type diff{ std::abs(arg.value() - arg2.value()) };
+        return diff < tolerance * std::numeric_limits<arg2_value_type>::epsilon();
+    }
+
+    MATCHER_P(UnitsEqUlp, arg2, std::string("Checks if two values with units are ") + std::string(negation ? "not " : "") + std::string("nearly equal (< 4ULP)"))
+    {
+        auto ulp_diff{ std::abs(boost::math::float_distance(arg.value(), arg2.value()))};
+        return ulp_diff < 4;
     }
 }
--- GTestMatcher/unit-tests.cpp
+++ GTestMatcher/unit-tests.cpp
@@ -18,12 +18,8 @@ using boost::units::cgs::centimeters;
 using ::testing::Not;
 using ::testing::Eq;
 using ::testing::UnitsEq;
+using ::testing::UnitsEqUlp;
 
-
-TEST(LengthTest, Unitless)
-{
-    EXPECT_THAT(1, Eq(1));
-}
 
 TEST(LengthTest, MetersOnly)
 {
@@ -37,11 +33,29 @@ TEST(LengthTest, MetersAndCentimeters)
     EXPECT_THAT(one_meter, UnitsEq(static_cast<decltype(one_meter)>(one_hundred_centimeters)));
 }
 
-EXPECT_DOUBLE_EQ()
-
 TEST(LengthTest, ApproximateMeters)
 {
     EXPECT_THAT(1.0 * meter, UnitsEq((1.0 + std::numeric_limits<double>::epsilon()) * meter));
     EXPECT_THAT(1.0 * meter, Not(UnitsEq(1.01 * meter)));
     EXPECT_THAT(1.0 * meter, Not(UnitsEq(1.1 * meter)));
+}
+
+
+TEST(LengthTestUlp, MetersOnly)
+{
+    EXPECT_THAT(1.0 * meter, UnitsEqUlp(1.0 * meter));
+}
+
+TEST(LengthTestUlp, MetersAndCentimeters)
+{
+    auto one_meter{ 1.0 * meter};
+    auto one_hundred_centimeters{ 100.0 * centimeters };
+    EXPECT_THAT(one_meter, UnitsEqUlp(static_cast<decltype(one_meter)>(one_hundred_centimeters)));
+}
+
+TEST(LengthTestUlp, ApproximateMeters)
+{
+    EXPECT_THAT(1.0 * meter, UnitsEqUlp((1.0 + std::numeric_limits<double>::epsilon()) * meter));
+    EXPECT_THAT(1.0 * meter, Not(UnitsEqUlp(1.01 * meter)));
+    EXPECT_THAT(1.0 * meter, Not(UnitsEqUlp(1.1 * meter)));
 }
